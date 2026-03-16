bb72b18fc0a11191f4eb9fa863b0122202d11a76
Luis Martinez <mluise2406@gmail.com>
2026-03-15 18:52:36 -0600

    authStore created

---
+++ src/stores/authStore.js
@@ -0,0 +1,61 @@
+import { defineStore } from 'pinia'
+import { jwtDecode } from 'jwt-decode'
+
+export const useAuthStore = defineStore("auth",{
+
+    state: () => ({
+        token: null,
+        user: null
+    }),
+
+    getters: {
+        isAuthenticated: (state) => !!state.token,
+        role: (state) => state.user?.role || null
+    },
+
+    actions: {
+        
+        setToken(token) {
+
+            this.token = token
+            localStorage.setItem("token", token)
+
+            try{
+                this.user = jwtDecode(tokem)
+            }catch {
+                this.logout()
+            }
+
+        },
+
+        loadFromStorage(){
+
+            const token = localStorage.getItem("token")
+            if(!token) return
+
+            try {
+                const decoded = jwtDecode(token)
+
+                if(decoded.exp * 1000 < Date.now()){
+                    this.logout()
+                    return
+                }
+
+                this.token = token
+                this.user = decoded
+            } catch {
+                this.logout()
+            }
+        },
+
+        logout() {
+            this.token = null,
+            this.user = null
+
+            localStorage.removeItem("token")
+        }
+
+    }
+
+
+})
